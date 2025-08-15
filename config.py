ff13012accc1d0eb267ed38e60be6f5cab3c44f8
avik <avikghosh9883@gmail.com>
2025-08-15 13:30:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/1223ed71ebdec8ead50b0-495ed65d1103f68a09.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/y267yo.mp4"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org/file/19a7a6330c9df2abee92e-4b078b22749c265232.jpg"
